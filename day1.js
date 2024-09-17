9f4294ada649c5be310883b0707e515878d736c7
Muhammad Saad Saleem <saadsaleemch@gmail.com>
2024-09-17 19:42:30 +0500

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,12 @@
+document.getElementById('demo2').style.fontSize = '35px';
+
+function myFunction() {
+    document.getElementById("demo4").innerHTML = "Boom! BOom!";
+}
+function myName() {
+    document.getElementById("demo5").innerHTML = "Her name is Bubbly"
+}
+function saad() {
+    document.write(143);
+}
+window.alert("Hi jana");
